60513584630a26655ed5e9cf277429dd4b986bc8
Ayub Bodirkhonov <ayub.bodirkhonov98@gmail.com>
2026-04-11 16:40:06 +0500

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,2 +1,22 @@
 
 export const cart = [];
+
+export function addToCart(productId) {
+    let matchingItem;
+
+    cart.forEach(cartItem => {
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+        }
+    })
+    //if similar item is in the cart, increase the quantity, otherwise add a new one
+    //matchingItem is an object which is truthy value
+    if (matchingItem) {
+        matchingItem.quantity++;
+    } else {
+        cart.push({
+            productId: productId,
+            quantity: 1
+        })
+    }
+}
